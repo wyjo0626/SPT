89f9b031d6eae9a5832645dd647fea2a4fe76a95
whdnjsdyd111 <whdnjsdyd111@naver.com>
2024-02-06 13:36:29 +0900

    pruned token save and load

--- peft/utils/save_and_load.py
+++ peft/utils/save_and_load.py
@@ -94,6 +94,7 @@ def get_peft_model_state_dict(
     elif config.peft_type == PeftType.ADAPTION_PROMPT:
         to_return = {k: state_dict[k] for k in state_dict if k.split(".")[-1].startswith("adaption_")}
     
+    
     elif config.is_prompt_learning:
         to_return = {}
         
@@ -102,6 +103,11 @@ def get_peft_model_state_dict(
         else:
             prompt_embeddings = model.get_prompt_embedding_to_save(adapter_name)
         to_return["prompt_embeddings"] = prompt_embeddings
+        
+        if config.peft_type == PeftType.XPROMPT_TUNING:
+            to_return["to_prune"] = model.prompt_encoder[adapter_name].to_prune
+            to_return["kept_prune"] = model.prompt_encoder[adapter_name].kept_prune
+            to_return["token_mask"] = model.prompt_encoder[adapter_name].token_mask
     else:
         raise NotImplementedError
     
@@ -193,6 +199,11 @@ def set_peft_model_state_dict(model, peft_model_state_dict, adapter_name="defaul
         model.prompt_encoder[adapter_name].embedding.load_state_dict(
             {"weight": peft_model_state_dict["prompt_embeddings"]}, strict=True
         )
+        
+        if config.peft_type == PeftType.XPROMPT_TUNING:
+            model.prompt_encoder[adapter_name].to_prune = peft_model_state_dict["to_prune"]
+            model.prompt_encoder[adapter_name].kept_prune = peft_model_state_dict["kept_prune"]
+            model.prompt_encoder[adapter_name].token_mask = peft_model_state_dict["token_mask"]
     
     return load_result
 
