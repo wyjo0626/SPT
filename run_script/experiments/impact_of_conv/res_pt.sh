f89c3e7f062c0d05735d2a3b269a29deb450c466
whdnjsdyd111 <whdnjsdyd111@naver.com>
2024-02-16 17:16:32 +0900

    impact of conv

---
+++ run_script/experiments/impact_of_conv/res_pt.sh
@@ -0,0 +1,48 @@
+export MODELS_NAME="bert-base-uncased t5-base"
+export TASK_NAME=super_glue
+export CUDA_VISIBLE_DEVICES=0
+export PEFT_TYPE=RESIDUAL_PROMPT_TUNING
+
+lr=1e-3
+max_seq_length=256
+bs=32
+epochs=20
+max_steps=3000
+weight_decay=1e-5
+virtual_tokens_list="10"
+
+for MODEL_NAME in $MODELS_NAME; do
+  for DATASET_NAME in boolq cb rte wic multirc; do
+    for seed in 42 43 44; do
+      for var in $virtual_tokens_list; do
+        if test "$DATASET_NAME" = "multirc"; then max_seq_length=348; fi
+        
+        python run.py \
+          --model_name_or_path $MODEL_NAME \
+          --run_name EXP1-$TASK_NAME-$DATASET_NAME-$MODEL_NAME-$lr-$PEFT_TYPE-$var-token-$seed \
+          --task_name $TASK_NAME \
+          --dataset_name $DATASET_NAME \
+          --do_train \
+          --do_eval \
+          --do_predict \
+          --per_device_train_batch_size $bs \
+          --per_device_eval_batch_size $bs \
+          --max_seq_length $max_seq_length \
+          --output_dir checkpoints/EXP1/$PEFT_TYPE/$MODEL_NAME/$TASK_NAME-$DATASET_NAME-$lr-$var-token-$seed/ \
+          --overwrite_output_dir \
+          --seed $seed \
+          --learning_rate $lr \
+          --save_strategy epoch \
+          --evaluation_strategy epoch \
+          --num_train_epochs $epochs \
+          --warmup_steps 500 \
+          --weight_decay $weight_decay \
+          --load_best_model_at_end \
+          --save_total_limit 1 \
+          --peft_type $PEFT_TYPE \
+          --num_virtual_tokens $var \
+          --encoder_bottleneck 250;
+      done;
+    done;
+  done;
+done;
