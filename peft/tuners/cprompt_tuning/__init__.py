e664ebeb9968c1c10e478037099067a1e8772776
whdnjsdyd111 <whdnjsdyd111@naver.com>
2024-02-25 22:27:22 +0900

    typo prompt init

--- peft/tuners/cprompt_tuning/__init__.py
+++ peft/tuners/cprompt_tuning/__init__.py
@@ -12,8 +12,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from .config import CPromptTuningConfig, CPromptTuningInit, CPromptTuningActivation
+from .config import CPromptTuningConfig, CPromptTuningActivation
 from .model import CPromptEmbedding
 
 
-__all__ = ["CPromptTuningConfig", "CPromptEmbedding", "CPromptTuningInit", "CPromptTuningActivation"]
+__all__ = ["CPromptTuningConfig", "CPromptEmbedding", "CPromptTuningActivation"]
