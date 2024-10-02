3de51b62b23a62073bf64b9cd2b058a91cfa628d
whdnjsdyd111 <whdnjsdyd111@naver.com>
2024-10-02 14:11:54 +0900

    P-Tuning encoder hidden size

--- run_script/peft/p-tuning/run_glue-t5.sh
+++ run_script/peft/p-tuning/run_glue-t5.sh
@@ -41,7 +41,8 @@ for MODEL_NAME in $MODELS_NAME; do
         --save_total_limit 1 \
         --peft_type $PEFT_TYPE \
         --init_type $init_type \
-        --num_virtual_tokens $virtual_token;
+        --num_virtual_tokens $virtual_token \
+        --encoder_hidden_size 128;
     done;
   done;
 done;
